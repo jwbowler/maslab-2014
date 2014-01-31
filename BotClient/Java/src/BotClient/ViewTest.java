0a8b6af92872b4ea3a5cdc1d4ada67c974e37ded
Gurtej Kanwar <gurtej@mit.edu>
2014-01-31 03:32:56 -0500

    Cleans up BotClient API

--- BotClient/Java/src/BotClient/ViewTest.java
+++ BotClient/Java/src/BotClient/ViewTest.java
@@ -6,26 +6,33 @@ import java.awt.image.BufferedImage;
 
 public class ViewTest {
 	public static void main( String[] args ) {
+				
+		BotClient botclient = new BotClient("18.150.7.174:6667","1221",false);
+		int timer = 0;
 		
-		// {"EViL":"admin", "1221":"jason", "0MtNBP10MG": "team1", "5byQh8Ly59": "team2", "b3MpHHs4J1":"team3", "a12L7plGB8":"team4", "H4Lx8c0mOw":"team5", "qXp0r37r9o":"team6", "mT82Qi240y":"team7", "YNxFh4tnOY":"team8", "14GHK83Vk6":"team9", "4LdcL0DQw4":"team10", "a0qzL4T9fq":"team11", "r5Z8L619ho":"team12", "i5d76YlHmB":"team13", "y8NKe1Oz54":"team14", "zP2Ki3R96P":"team15", "Nj5fd3q7pe":"team16", "fBETY83RSp":"team17"}
-		
-		BotClient botclient = new BotClient("18.150.7.174:6667","zP2Ki3R96P",false);
-		
-		botclient.send("a", "State", "Ball Search");
-		botclient.send("b", "Gyro", "4.14159");
-		
-		// Smiley
-		BufferedImage image = new BufferedImage(320,240,BufferedImage.TYPE_4BYTE_ABGR);
-		Graphics g = image.getGraphics();
-		int WD = 320/8;
-		for ( int i = 0; i < 8; i++ ) {
-			Color col = Color.getHSBColor(i/8.0f,1.0f,1.0f);
-			g.setColor(col);
-			g.fillRect(i*WD,0,WD,240);
+		while (true) {
+            botclient.send("a", "State", "Ball Search");
+            botclient.send("b", "Gyro", String.format("%f", Math.random()));
+    
+    		// Smiley
+    		BufferedImage image = new BufferedImage(320,240,BufferedImage.TYPE_4BYTE_ABGR);
+    		Graphics g = image.getGraphics();
+    		int WD = 320/8;
+    		for ( int i = 0; i < 8; i++ ) {
+    			Color col = Color.getHSBColor(((i+timer)%8)/8.0f,1.0f,1.0f);
+    			g.setColor(col);
+    			g.fillRect(i*WD,0,WD,240);
+    		}
+    		timer++;
+    		
+    		botclient.sendImage(image);
+    		
+    		// NOTE: For the sake of streaming data more smoothly to BotClient, and avoiding hogging
+    		// your own processor, we recommend streaming data at about 1Hz.
+    		try {
+    		    Thread.sleep(1000);
+    		}
+    		catch (Exception e) {}
 		}
-		
-		botclient.sendImage(image);
-		
-		botclient.close();
 	}
 }
